87ff5432748bd3857167a34ffc155719dd039953
Kim Chanho <kiss.yagni.dry@gmail.com>
2023-02-19 15:59:42 +0900

    Practice dynamic class, component, prop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
+	import ContactCard from './ContactCard.svelte';
+	import Goal from './Goal.svelte';
+
 	let name = 'Max';
 	let age = 30;
+	let image = '';
+	let jobName = 'System Engineer';
+	let description = 'Since 08.Oct.2022, working as a System Engineer in Tokyo';
+	let goal = '';
+
 	$: uppercaseName = name.toUpperCase();
+	$: console.log(name);
+	$: if (name === 'chanho') {
+		age = 26;
+	}
 
 	function incrementAge() {
 		age += 1;
@@ -9,11 +21,26 @@
 	function changeName() {
 		name = 'chanho';
 	}
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
 </script>
 
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
-<button on:click={changeName}>Change Name</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<!-- <input type="text" on:input={nameInput} value={name} /> -->
+<input type="text" bind:value={name} />
+<input type="text" bind:value={jobName} />
+<input type="text" bind:value={image} />
+<textarea bind:value={description} />
+
+<input type="text" bind:value={goal} />
+
+<ContactCard userName={name} {jobName} {description} userImage={image} />
+
+<Goal {goal} />
 
 <style>
 	h1 {
---
+++ src/routes/Goal.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let goal;
+</script>
+
+<h1 class:containExclamation={goal.includes('!')}>{goal}</h1>
+
+<style>
+	.containExclamation {
+		color: red;
+	}
+</style>
